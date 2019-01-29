ef3b474ee32dc8b35a0c5ca59dee3ca026ef91e2
clementsem <clementctes@gmail.com>
2019-01-29 09:45:36 +0100

    js3exo3

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+function green() {
+ document.getElementById('text').style.color = "green"
+}
+function blue() {
+ document.getElementById('text').style.color = "blue"
+}
+function red() {
+ document.getElementById('text').style.color = "red"
+}
